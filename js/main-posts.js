dd8d9de4f3c2472c3fd187db39346a7cadc3fdee
Ahmed Nassar <108141702+Ahmed-Mohamed-Nassar@users.noreply.github.com>
2024-06-02 13:19:47 -0700

    add show comments
    add show comments

--- js/main-posts.js
+++ js/main-posts.js
@@ -62,6 +62,20 @@ function appendPostToContainer(post) {
           </svg>
           <p class="card-text ms-2"><small class="text-body-secondary postCreated_at">Last updated ${postCreated_at}</small></p>
         </div>
+
+
+        <div class="row" style="display: flex; flex-direction: column; padding: 10px 15px;"> 
+        <p class="card-text ms-2 " style=" display: flex; margin-top: 10px;margin-bottom: 10px; justify-content: space-between; align-items: center;">
+        <button class="showComments" value="${post.id}" style="border: 1px solid black; background-color: transparent; border-radius: 6px 6px; padding: 5px 10px; margin-right: 5px;">show ${post.comments_count} comments</button>
+        
+        <button class="showComments" value="${post.id}" style="border: 1px solid black; background-color: transparent; border-radius: 6px 6px;
+        padding: 5px 10px;">show ${post.comments_count} comments</button>
+        </p>
+        
+        </div>
+
+
+
       </div>
     </div>
   `;
@@ -104,6 +118,7 @@ function addNewPosts() {
         appendPostToContainer(post);
       }
       loadingPosts = false;
+      console.log(posts);
     })
     .catch((error) => {
       console.log(error);
@@ -139,7 +154,11 @@ function getPosts(limit) {
         let postImg = "";
         let postCreated_at = "";
         postUsername = post.author.name;
+        // postImg = post.image;
         postImg = post.image;
+        if (isEmpty(post.image)) {
+          postImg = "";
+        }
         postCreated_at = post.created_at;
 
         if (isEmpty(post.author.profile_image)) {
@@ -153,10 +172,14 @@ function getPosts(limit) {
         if (post.body != null) {
           postBody = post.body;
         }
-        theContainerOfPosts.innerHTML += `
+        //
+        if (postImg !== "") {
+          theContainerOfPosts.innerHTML += `
           <div class="card mb-5" style="width: 40rem; max-width:100%">
             <div class="userInfo p-3 d-flex justify-content-start align-items-center">
+
               <img class="postUserimg" src=${postUserimg} alt="" style="width: 50px; height: 50px; border-radius: 50%;">
+
               <h5 class="postUsername" style="padding-left: 10px;">${postUsername}</h5>
             </div>
             <img  src=${postImg} class="card-img-top p-2 postImg" alt="">
@@ -167,10 +190,55 @@ function getPosts(limit) {
               <div class="comment  d-flex justify-content-start align-items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#493489" d="M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9c.1-.2 .2-.3 .3-.5z"/></svg>
                 <p class="card-text ms-2"><small class="text-body-secondary postCreated_at">Last updated ${postCreated_at}</small></p>
-              </div>
+                </div>
+
+<div class="row" style="display: flex; flex-direction: column; padding: 10px 15px;"> 
+<p class="card-text ms-2 " style=" display: flex; margin-top: 10px;margin-bottom: 10px; justify-content: space-between; align-items: center;">
+<button class="showComments" value="${post.id}" style="border: 1px solid black; background-color: transparent; border-radius: 6px 6px;
+padding: 5px 10px;margin-right: 5px; ">show ${post.comments_count} comments</button>
+
+<button class="showComments" value="${post.id}" style="border: 1px solid black; background-color: transparent; border-radius: 6px 6px;
+padding: 5px 10px;">show ${post.comments_count} comments</button>
+</p>
+
+</div>
             </div>
           </div>
           `;
+        } else {
+          theContainerOfPosts.innerHTML += `
+          <div class="card mb-5" style="width: 40rem; max-width:100%">
+            <div class="userInfo p-3 d-flex justify-content-start align-items-center">
+
+              <img class="postUserimg" src=${postUserimg} alt="" style="width: 50px; height: 50px; border-radius: 50%;">
+
+              <h5 class="postUsername" style="padding-left: 10px;">${postUsername}</h5>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title postTitle">${postTitle}</h5>
+              <p class="card-text postBody">${postBody}</p>
+              <hr>
+              <div class="comment  d-flex justify-content-start align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#493489" d="M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9c.1-.2 .2-.3 .3-.5z"/></svg>
+                <p class="card-text ms-2"><small class="text-body-secondary postCreated_at">Last updated ${postCreated_at}</small></p>
+                </div>
+
+<div class="row" style="display: flex; flex-direction: column; padding: 10px 15px;"> 
+<p class="card-text ms-2 " style=" display: flex; margin-top: 10px;margin-bottom: 10px; justify-content: space-between; align-items: center;">
+<button class="showComments" value="${post.id}" style="border: 1px solid black; background-color: transparent; border-radius: 6px 6px;
+padding: 5px 10px;">show ${post.comments_count} comments</button>
+
+<button class="showComments" value="${post.id}" style="border: 1px solid black; background-color: transparent; border-radius: 6px 6px;
+padding: 5px 10px;">show ${post.comments_count} comments</button>
+</p>
+
+</div>
+            </div>
+          </div>
+          `;
+        }
+
+        //
       }
     })
     .catch((error) => {
@@ -284,3 +352,58 @@ function createPostAndGetPostsAgain() {
 //   });
 // }
 // updateProfile();
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("showComments")) {
+    // console.log("good");
+    // console.log(e.target.value);
+    let commentId = e.target.value;
+    let btn = e.target;
+    axios
+      .get(`${url}/posts/${commentId}`)
+      .then((response) => {
+        let commentsArray = response.data.data.comments;
+
+        let parent = e.target.parentElement.parentElement;
+
+        if (commentsArray.length > 0) {
+          // console.log(commentsArray);
+          btn.setAttribute("disabled", "");
+
+          for (const comment of commentsArray) {
+            console.log(comment);
+            let commentProfileImgUrl = comment.author.profile_image;
+            console.log(commentProfileImgUrl);
+            let commentProfileName = comment.author.name;
+            console.log(commentProfileName);
+            let commentBody = comment.body;
+            console.log(commentBody);
+            // console.log(e.target.parentElement.parentElement);
+            if (isEmpty(commentProfileImgUrl)) {
+              commentProfileImgUrl = "../imgs/prof.png";
+            }
+            parent.innerHTML += `<div style="display:flex; margin-bottom: 5px; background-color: #f9f9f9; padding: 10px 15px; border: 1px solid gray; border-radius: 10px 10px; margin-top: 5px;">
+            <div style="margin-right:5px">
+              <img style ="width: 50px; height: 50px; border-radius: 50%;"src="${commentProfileImgUrl}" alt="" srcset="">
+            </div>
+
+            <div>
+            <div style="font-weight:bold">${commentProfileName}</div>
+            <div>Comment: ${commentBody}</div>
+            </div>
+
+            </div>`;
+          }
+        } else {
+          btn.setAttribute("disabled", "");
+          let parent = e.target.parentElement.parentElement;
+
+          parent.innerHTML += `<div style="display:flex; margin-bottom: 5px; background-color: #f9f9f9; padding: 10px 15px; border: 1px solid gray; border-radius: 10px 10px; margin-top: 5px;">
+            <div style="font-weight:bold">This Post Have No Comment</div>
+            </div>`;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
